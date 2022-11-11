295610a9885b55796a55df98fc03767b61f3875b
drymarchonshaun <draedena.miller@gmail.com>
2022-11-11 13:46:37 -0800

    new file:   loader/entries/archrecovery.conf 	modified:   main.sh 	deleted:    recovery/loader/entries/archrecovery.conf

--- main.sh
+++ main.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/fish
-mkdir /recovery
-mount $RECOVERY_PARTITION /recovery
-curl https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso --output /$RECOVERY_PARTITION/archlinux-x86_64.iso
-mount /$RECOVERY_PARTITION/archlinux-x86_64.iso /tmp/iso
-cp -r /tmp/iso/arch/boot/x86_64/* /recovery/
+
+set BOOT_PARTITION (mount | grep /boot | awk -F" " '{ print $1 }') 
+curl https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso --output /$BOOT_PARTITION/recovery/archlinux-x86_64.iso
+mount /$BOOT_PARTITION/recovery/archlinux-x86_64.iso /tmp/iso
+cp -r /tmp/iso/arch/boot/x86_64/* /boot/recovery/
 umount /tmp/iso
-git clone https://github.com/DrymarchonShaun/ArchRecovery /tmp/ArchRecovery/
-cp /tmp/ArchRecovery/recovery/loader/``
+curl https://raw.githubusercontent.com/DrymarchonShaun/ArchRecovery/master/recovery/loader/entries/archrecovery.conf > sed s/BOOT_PARTITION/($BOOT_PARTITION)/g | touch /boot/loader/entries/archrecovery.conf

efe98aab540fe2aa0678f53e4dfebc3e48bc05d9
drymarchonshaun <draedena.miller@gmail.com>
2022-11-11 13:15:12 -0800

    new file:   main.sh 	new file:   recovery/loader/entries/archrecovery.conf 	new file:   recovery/loader/entries/isoboot.conf 	new file:   recovery/loader/entries/squashfsboot.conf

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/fish
+mkdir /recovery
+mount $RECOVERY_PARTITION /recovery
+curl https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso --output /$RECOVERY_PARTITION/archlinux-x86_64.iso
+mount /$RECOVERY_PARTITION/archlinux-x86_64.iso /tmp/iso
+cp -r /tmp/iso/arch/boot/x86_64/* /recovery/
+umount /tmp/iso
+git clone https://

311fefa550c5a7d964059802ba1d49dc44469846
drymarchonshaun <draedena.miller@gmail.com>
2022-11-11 13:27:54 -0800

    renamed:    recovery/loader/entries/isoboot.conf -> isoboot.conf 	modified:   main.sh 	renamed:    recovery/loader/entries/squashfsboot.conf -> squashfsboot.conf

--- main.sh
+++ main.sh
@@ -5,4 +5,5 @@ curl https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso --output /$
 mount /$RECOVERY_PARTITION/archlinux-x86_64.iso /tmp/iso
 cp -r /tmp/iso/arch/boot/x86_64/* /recovery/
 umount /tmp/iso
-git clone https://
+git clone https://github.com/DrymarchonShaun/ArchRecovery /tmp/ArchRecovery/
+cp /tmp/ArchRecovery/recovery/loader/``

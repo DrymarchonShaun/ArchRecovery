b00d40a90cbd760a2e45e80e069aa9e97eecf81e
drymarchonshaun <draedena.miller@gmail.com>
2022-11-11 13:55:07 -0800

    modified:   main.sh

--- main.sh
+++ main.sh
@@ -5,4 +5,5 @@ curl https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso --output /$
 mount /$BOOT_PARTITION/recovery/archlinux-x86_64.iso /tmp/iso
 cp -r /tmp/iso/arch/boot/x86_64/* /boot/recovery/
 umount /tmp/iso
-curl https://raw.githubusercontent.com/DrymarchonShaun/ArchRecovery/master/recovery/loader/entries/archrecovery.conf > sed s/BOOT_PARTITION/($BOOT_PARTITION)/g | touch /boot/loader/entries/archrecovery.conf
+curl https://raw.githubusercontent.com/DrymarchonShaun/ArchRecovery/master/loader/entries/archrecovery.conf --output /boot/loader/entries/archrecovery.conf
+sed -i "s#BOOT_PARTITION#$BOOT_PARTITION#g" /boot/loader/entries/archrecovery.conf
